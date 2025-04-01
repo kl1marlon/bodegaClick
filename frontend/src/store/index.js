6aa9f8bf7668eaa09ee015e9f92dfe2ba2fc160f
kl1marlon <marlonccs94@gmail.com>
2025-04-01 10:12:04 -0400

    fix6

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,7 +3,7 @@ import productosReducer from './productosSlice';
 import tasasCambioReducer from './tasasCambioSlice';
 import facturasReducer from './facturasSlice';
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     productos: productosReducer,
     tasasCambio: tasasCambioReducer,
