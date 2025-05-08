c1407735432640db2d1ca5247afef6ba1694658b
kl1marlon <marlonccs94@gmail.com>
2025-05-08 19:55:44 -0400

    cambios

--- frontend/src/pages/NuevaFactura.js
+++ frontend/src/pages/NuevaFactura.js
@@ -224,7 +224,7 @@ const NuevaFactura = () => {
       precio_compra_usd: precio_compra,
       unidades_paquete: unidades,
       precio_unitario: precio_venta,
-      total: precio_venta * cantidad,
+      total: precio_compra * cantidad,
       porcentajeGanancia: porcentaje,
       aplicarIva: productoEditando.aplicarIva,
       precio_base_usd: precio_base_usd,  // Nuevo campo
