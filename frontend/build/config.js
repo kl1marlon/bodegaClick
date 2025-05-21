0d78718d9224355b651aa67a632a1da4111ef146
Marlon <kl1marlon@192.168.1.11>
2025-05-20 20:59:15 -0400

    login2

---
+++ frontend/build/config.js
@@ -0,0 +1,5 @@
+// Configuración dinámica para entornos de producción
+// En producción, este archivo será reemplazado por un script generado en el entrypoint del contenedor
+window.ENV = { 
+  API_URL: "https://bodegaclick-production.up.railway.app/api" 
+}; 
